cfdac9cbdea7a6a3be1773dec9ef8c7741886dfd
William Wong <compulim@hotmail.com>
2023-02-12 12:41:07 -0800

    Fix ESLint

--- packages/react-wrap-with/src/wrapWith.simple.spec.tsx
+++ packages/react-wrap-with/src/wrapWith.simple.spec.tsx
@@ -1,6 +1,6 @@
 /** @jest-environment jsdom */
 
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import React from 'react';
 
 import wrapWith from './wrapWith';
