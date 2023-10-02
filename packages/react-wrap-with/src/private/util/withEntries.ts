ff3dfe8a6b2cbb1a48537209ded791282325ad7e
William Wong <compulim@hotmail.com>
2023-10-02 00:54:42 -0700

    Remove unnecessary

--- packages/react-wrap-with/src/private/util/withEntries.ts
+++
@@ -1,3 +0,0 @@
-export default function filterEntries<T extends object>(obj: T, predicate: (value: unknown, key: keyof T) => boolean) {
-  return Object.fromEntries(Object.entries(obj).filter(([key, value]) => predicate(value, key as keyof T)));
-}
