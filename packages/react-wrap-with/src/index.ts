63c63bc5431fedc364f6d21e1b897a3ddc61c06a
William Wong <compulim@hotmail.com>
2023-10-09 01:31:47 -0700

    Export type-fest

--- packages/react-wrap-with/src/index.ts
+++ packages/react-wrap-with/src/index.ts
@@ -3,9 +3,11 @@ import Spy from './Spy';
 import withProps from './withProps';
 import wrapWith from './wrapWith';
 
+// Due to [a TypeScript bug with monorepo](https://github.com/microsoft/TypeScript/issues/47663), we need to export types from dependencies.
+import type { ConditionalKeys, Simplify } from 'type-fest';
 import type { HowOf } from './HowOf';
 import type { PropsOf } from './PropsOf';
 import type { RefOf } from './RefOf';
 
-export { Extract, Spy, withProps, wrapWith };
+export { ConditionalKeys, Extract, Simplify, Spy, withProps, wrapWith };
 export { type HowOf, type PropsOf, type RefOf };
