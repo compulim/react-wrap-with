527c6d4a4d79d9c2d7667862ce0ae046b7ad2435
William Wong <compulim@hotmail.com>
2024-03-31 14:11:10 -0700

    Fix expect error message

--- packages/react-wrap-with/__tests__/types/pipe.extraneous.tsx
+++ packages/react-wrap-with/__tests__/types/pipe.extraneous.tsx
@@ -12,5 +12,5 @@ const Hello = () => <div>Hello, World!</div>;
 
 const HelloWithEffect = wrapWith(Effect, { effect: Extract })(Hello);
 
-// @ts-expect-error ["Property 'value' does not exist on type 'IntrinsicAttributes & { effect: string; ref?: LegacyRef<unknown> | undefined; key?: Key | null | undefined; }'.", "Property 'value' does not exist on type 'IntrinsicAttributes & { effect: string; ref?: Ref<unknown> | undefined; key?: Key | null | undefined; }'."]
+// @ts-expect-error ["Property 'value' does not exist on type 'IntrinsicAttributes & { effect: string; ref?: LegacyRef<unknown> | undefined; key?: Key | null | undefined; }'.", "Property 'value' does not exist on type 'IntrinsicAttributes & { effect: string; ref?: Ref<unknown> | undefined; key?: Key | null | undefined; } & { children?: ReactNode; }'."]
 <HelloWithEffect effect="blink" value="Hello, World!" />;
