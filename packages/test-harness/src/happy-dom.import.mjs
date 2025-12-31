0102e3e696b73376413f734eeece78db47f97311
William Wong <compulim@users.noreply.github.com>
2025-12-31 22:39:51 +0000

    Clean up

--- packages/test-harness/src/happy-dom.import.mjs
+++ packages/test-harness/src/happy-dom.import.mjs
@@ -1,12 +1,11 @@
 import { GlobalRegistrator } from '@happy-dom/global-registrator';
-import { after, afterEach, before, beforeEach } from 'node:test';
+import { after, before } from 'node:test';
 
 // `node --import` does passthrough things set on `globalThis` to the test.
 // However, `node --test-global-setup` will not passthrough. The test will run without `globalThis.document`.
 // Not sure if this is intentional or not. We must load this module using `node --import`.
 
 before(() => {
-  console.log('REGISTER');
   GlobalRegistrator.register({
     height: 1080,
     url: 'http://localhost:3000',
@@ -15,11 +14,8 @@ before(() => {
 });
 
 after(async () => {
-  if (GlobalRegistrator.isRegistered) {
-    // Must unregister, otherwise, there will be promises lingering.
-    await GlobalRegistrator.unregister();
-    console.log('UNREGISTERED');
-  }
+  // Must unregister, otherwise, there will be promises lingering.
+  GlobalRegistrator.isRegistered && (await GlobalRegistrator.unregister());
 });
 
 export {};
